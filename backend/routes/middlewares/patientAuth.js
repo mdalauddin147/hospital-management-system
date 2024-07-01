9c9cf1c47c83eb78092590231adff0984c1c1c32
mdalauddin147 <82751300+mdalauddin147@users.noreply.github.com>
2024-07-01 20:24:49 +0545

    initial commit

---
+++ backend/routes/middlewares/patientAuth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+function patientAuth(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  jwt.verify(token, process.env.SECRET_KEY, (err, payload) => {
+    if (err) {
+      console.log("ERROR",err)
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    console.log("PAYLOAD ", payload);
+    if (payload.userType == "Patient") {
+      next();
+    } else {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  });
+}
+
+module.exports = patientAuth;
